24de3de7d8e7a589422d1518aed2301c48b5176b
Arnau Mora <arnyminerz@proton.me>
2024-05-12 15:14:10 +0200

    Fixed uptime observance
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/main/kotlin/server/MQTT.kt
+++ src/main/kotlin/server/MQTT.kt
@@ -3,6 +3,7 @@ package server
 import java.nio.charset.Charset
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -18,7 +19,11 @@ import org.eclipse.paho.client.mqttv3.MqttMessage
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
 object MQTT {
+    private const val UPTIME_TOPIC = "\$SYS/broker/uptime"
+
     private lateinit var client: MqttClient
+
+    private var uptimeSupervisionJob: Job? = null
 
     private val messagesFlow = MutableStateFlow<Pair<String, MqttMessage>?>(null)
 
@@ -40,47 +45,60 @@ object MQTT {
 
                 override fun messageArrived(topic: String, message: MqttMessage?) {
                     if (message != null) {
-                        println("::MQTT:: Received new message in $topic")
+                        // Only log messages that are not system messages
+                        if (!topic.startsWith('$')) {
+                            println("::MQTT:: Received new message in $topic.")
+                        }
                         messagesFlow.tryEmit(topic to message)
                     }
                 }
             }
         )
-
-        CoroutineScope(Dispatchers.IO).launch {
-            subscribe("\$SYS/broker/uptime").collect { message ->
-                val payload = message.payload
-                val msg = payload.toString(Charset.defaultCharset())
-                _uptimeFlow.tryEmit(msg.substringBefore(' ').toInt())
-            }
-        }
     }
 
     fun isConnected(): Boolean = client.isConnected
 
     fun connect() {
+        if (isConnected()) return
         client.connect()
         println("::MQTT:: Connected")
+
+        uptimeSupervisionJob = CoroutineScope(Dispatchers.IO).launch {
+            subscribe(UPTIME_TOPIC).collect { message ->
+                val payload = message.payload
+                val msg = payload.toString(Charset.defaultCharset())
+                val uptime = msg.substringBefore(' ').toInt()
+                println("Uptime: $uptime seconds")
+                _uptimeFlow.tryEmit(uptime)
+            }
+        }
     }
 
     fun disconnect() {
+        if (!isConnected()) return
         client.disconnect()
         println("::MQTT:: Disconnected")
+
+        uptimeSupervisionJob?.cancel()
+        uptimeSupervisionJob = null
     }
 
     fun publish(topic: String, message: String) {
         if (!isConnected()) error("MQTT client is not connected")
 
-        val msg = MqttMessage(message.toByteArray()).apply {
+        val payload = message.encodeToByteArray()
+        val msg = MqttMessage(payload).apply {
             qos = 2
         }
         client.publish(topic, msg)
+        println("::MQTT:: Published ${payload.size} bytes to $topic")
     }
 
     fun subscribe(topic: String): Flow<MqttMessage> {
         if (!isConnected()) error("MQTT client is not connected")
 
         client.subscribe(topic)
+        println("::MQTT:: Subscribed to $topic")
 
         return messagesFlow.filter { it?.first == topic }.map { it!!.second }.cancellable()
     }
